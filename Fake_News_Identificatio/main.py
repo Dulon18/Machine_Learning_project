989d86203e59c47f9747292934861eb3ec07a1cc
Dulon <80118217+Dulon18@users.noreply.github.com>
2021-08-13 18:54:56 +0600

    Update main.py

--- Fake_News_Identificatio/main.py
+++ Fake_News_Identificatio/main.py
@@ -43,7 +43,7 @@ def stemming(content):
     stemmed_content = re.sub('[^a-zA-Z]',' ',content)
     stemmed_content = stemmed_content.lower()
     stemmed_content = stemmed_content.split()
-    stemmed_content = [port_stem.stem(word) for word in stemmed_content if not word in stopwords.words('english')                   ]
+    stemmed_content = [port_stem.stem(word) for word in stemmed_content if not word in stopwords.words('english')]
     stemmed_content = ' '.join(stemmed_content)
     return stemmed_content
 
@@ -58,7 +58,6 @@ y=dataset['label'].values
 print(x,y)
 
 # converting the textual data to numerical data
-
 vectorizer = TfidfVectorizer()
 vectorizer.fit(x)
 x = vectorizer.transform(x)
